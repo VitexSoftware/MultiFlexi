8a3ecc893168e8b8f16aba044bd081d1189fe9e3
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-07-03 23:24:48 +0200

    Do not include any external php code into phinx-adapter

--- debian/conf/phinx-adapter.php
+++ debian/conf/phinx-adapter.php
@@ -7,25 +7,52 @@
  * @copyright  2021 Vitex Software
  */
 
-if(file_exists('/var/lib/composer/multi-abraflexi-setup/autoload.php')){
-    include_once '/var/lib/composer/multi-abraflexi-setup/autoload.php';
-} else {
-    include_once '/usr/share/php/EaseCore/Atom.php';
-    include_once '/usr/share/php/EaseCore/Functions.php';
-    include_once '/usr/share/php/EaseCore/Shared.php';
+/**
+ * Get configuration from constant or environment
+ * 
+ * @param string $constant
+ * 
+ * @return string
+ */
+function cfg($constant) {
+    $cfg = null;
+    if (!empty($constant) && defined($constant)) {
+        $cfg = constant($constant);
+    } elseif (isset($_ENV) && array_key_exists($constant, $_ENV)) {
+        $cfg = $_ENV[$constant];
+    } elseif (($env = getenv($constant)) && !empty($env)) {
+        $cfg = getenv($constant);
+    }
+    return $cfg;
 }
 
-$shared = \Ease\Shared::instanced();
+/**
+ * Load Configuration values from json file $this->configFile and define UPPERCASE keys
+ *
+ * @param string  $configFile      Path to file with configuration
+ *
+ * @return array full configuration array
+ */
+function loadConfig($configFile, $defineConstants) {
+    foreach (file($configFile) as $cfgRow) {
+        if (strchr($cfgRow, '=')) {
+            list($key, $value) = explode('=', $cfgRow);
+            $configuration[$key] = trim($value, " \t\n\r\0\x0B'\"");
+            define($key, $configuration[$key]);
+        }
+    }
+}
+
 if (file_exists('/etc/multi-abraflexi-setup/.env')) {
-    $shared->loadConfig('/etc/multi-abraflexi-setup/.env', true);
+    loadConfig('/etc/multi-abraflexi-setup/.env', true);
 }
 
 $prefix = "/usr/lib/multi-abraflexi-setup/db/";
 
 $sqlOptions = [];
 
-if (strstr(\Ease\Functions::cfg('DB_CONNECTION'), 'sqlite')) {
- $sqlOptions["database"] = "/var/lib/dbconfig-common/sqlite3/multi-abraflexi-setup/".basename(\Ease\Functions::cfg("DB_DATABASE"));
+if (strstr(cfg('DB_CONNECTION'), 'sqlite')) {
+    $sqlOptions["database"] = "/var/lib/dbconfig-common/sqlite3/multi-abraflexi-setup/" . basename(cfg("DB_DATABASE"));
 }
 $engine = new \Ease\SQL\Engine(null, $sqlOptions);
 $cfg = [
