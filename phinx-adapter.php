5e3523f001213571cac0a4549ed19a96e5fc7aec
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-07-03 20:25:10 +0200

    try to hotfix include problem

--- phinx-adapter.php
+++ phinx-adapter.php
@@ -1,9 +1,4 @@
 <?php
-
-if(file_exists('/usr/lib/multi-abraflexi-setup/phinx-adapter.php')){
-    fpassthru('/usr/lib/multi-abraflexi-setup/phinx-adapter.php');
-}
-
 /**
  * Multi AbraFlexi Setup - Phinx database adapter.
  *
