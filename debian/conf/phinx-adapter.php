0c87101e5d87375020bdec1c2248e3524ec335fb
Vítězslav Dvořák <info@vitexsoftware.cz>
2022-11-01 10:06:53 +0100

    ToMemory logger dependency added

--- debian/conf/phinx-adapter.php
+++ debian/conf/phinx-adapter.php
@@ -14,6 +14,7 @@ include_once '/usr/share/php/EaseCore/Molecule.php';
 include_once '/usr/share/php/EaseCore/Logger/Logging.php';
 include_once '/usr/share/php/EaseCore/Logger/ToStd.php';
 include_once '/usr/share/php/EaseCore/Logger/ToSyslog.php';
+include_once '/usr/share/php/EaseCore/Logger/ToMemory.php';
 include_once '/usr/share/php/EaseCore/Sand.php';
 include_once '/usr/share/php/EaseCore/Functions.php';
 include_once '/usr/share/php/EaseCore/Logger/Loggingable.php';
