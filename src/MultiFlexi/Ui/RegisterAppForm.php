b70e2eb58929aa0343a6b66c0e74cbf37719929a
CyberVitexus <info@vitexsoftware.cz>
2023-11-06 22:33:21 +0100

    deploy & homepage fields added to app fields

--- src/MultiFlexi/Ui/RegisterAppForm.php
+++ src/MultiFlexi/Ui/RegisterAppForm.php
@@ -25,13 +25,15 @@ class RegisterAppForm extends EngineForm
     public function afterAdd()
     {
         $this->setTagProperty('enctype', 'multipart/form-data');
-        $this->addInput(new InputTextTag('nazev'), _('Application name'));
-        $this->addInput(new InputTextTag('popis'), _('Application Description'));
+        $this->addInput(new InputTextTag('name'), _('Application name'));
+        $this->addInput(new InputTextTag('description'), _('Application Description'));
         $this->addInput(new InputTextTag('executable'), _('Path to binary'));
         $this->addInput(new InputTextTag('setup'), _('Setup Command'), '', _('Command used to setup new company for use with command'));
         $this->addInput(new InputTextTag('cmdparams'), _('Command arguments'), '', _('you can use macros like {ABRAFLEXI_URL} or custom defined config fields.'));
         $imgInput = $this->addInput(new InputFileTag('imageraw'), _('Application Icon'));
         $this->addInput(new Toggle('enabled', $this->engine->getDataValue('enabled') == 1), _('Enabled'));
+        $this->addInput(new InputTextTag('homepage'), _('Application Homepage'));
+        $this->addInput(new InputTextTag('deploy'), _('Application deploy command'));
         $this->addInput(new SubmitButton(_('Save'), 'success'));
         if (!is_null($this->engine->getDataValue('id'))) {
             $this->addItem(new InputHiddenTag('id'));
