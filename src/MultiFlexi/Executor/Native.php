4c94a81215e68228eee7ded2c8859cce33b90d54
CyberVitexus <info@vitexsoftware.cz>
2023-11-17 21:05:04 +0100

    Base for Several Executors Base for Podman support

---
+++ src/MultiFlexi/Executor/Native.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Multi Flexi -
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ * @copyright  2023 Vitex Software
+ */
+
+namespace MultiFlexi\Excutor;
+
+/**
+ * Description of Native
+ *
+ * @author vitex
+ */
+class Native implements \MultiFlexi\Executor
+{
+    //put your code here
+}
---
+++ src/MultiFlexi/Executor/Podman.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Multi Flexi - Run Tasks in container
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ * @copyright  2023 Vitex Software
+ */
+
+namespace MultiFlexi\Excutor;
+
+/**
+ * Description of Podman
+ *
+ * @author vitex
+ */
+class Podman implements \MultiFlexi\Executor
+{
+    //put your code here
+}
