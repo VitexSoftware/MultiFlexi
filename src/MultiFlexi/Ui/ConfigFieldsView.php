23c489e1b17f170ac2c239c178bdb485bf74b097
CyberVitexus <info@vitexsoftware.cz>
2023-11-21 23:23:17 +0100

    Nicest App Config page

---
+++ src/MultiFlexi/Ui/ConfigFieldsView.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace MultiFlexi\Ui;
+
+/**
+ * Description of ConfiguredFieldBadges
+ *
+ * @author vitex
+ */
+class ConfigFieldsView extends \Ease\Html\DivTag
+{
+    /**
+     *
+     * @param array $fieldsInfo
+     */
+    public function __construct(array $fieldsInfo)
+    {
+        parent::__construct();
+        foreach ($fieldsInfo as $key => $fieldInfo) {
+            $this->addItem(self::confInfo($fieldInfo));
+        }
+    }
+
+    public static function confInfo($fieldInfo)
+    {
+        $container = new \Ease\TWB4\Container($fieldInfo['type']);
+        $container->addItem(new \Ease\Html\H3Tag($fieldInfo['keyname']));
+        $container->addItem($fieldInfo['description']);
+
+        $dl = new \Ease\Html\DlTag();
+        $dl->addDef(_('Default value'), $fieldInfo['defval']);
+        $dl->addDef(_('Requied'), $fieldInfo['required'] ? '✅' : '❎');
+        $container->addItem($dl);
+        return new \Ease\TWB4\Card($container);
+    }
+}
