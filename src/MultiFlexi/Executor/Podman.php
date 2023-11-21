b93da1c9fe97fa40941cfd406b07462409929fce
CyberVitexus <info@vitexsoftware.cz>
2023-11-21 10:00:17 +0100

    Preparations for several run methods

--- src/MultiFlexi/Executor/Podman.php
+++ src/MultiFlexi/Executor/Podman.php
@@ -16,7 +16,55 @@ namespace MultiFlexi\Excutor;
  *
  * @author vitex
  */
-class Podman implements \MultiFlexi\Executor
+class Podman extends \Ease\Sand implements \MultiFlexi\executor
 {
-    //put your code here
+    use \Ease\Logger\Logging;
+
+    const PULLCOMMAND = 'podman pull docker.io/vitexsoftware/debian:bookworm';
+
+    public $job;
+
+    public function __construct($job)
+    {
+        $this->job = $job;
+    }
+
+    public function launch()
+    {
+        $this->pullImage();
+        $this->launchContainer();
+        $this->updateContainer();
+        $this->deployApp();
+        $this->runApp();
+        $this->storeLogs();
+        $this->stopContainer();
+    }
+
+    public function pullImage()
+    {
+    }
+
+    public function launchContainer()
+    {
+    }
+
+    public function updateContainer()
+    {
+    }
+
+    public function deployApp()
+    {
+    }
+
+    public function runApp()
+    {
+    }
+
+    public function storeLogs()
+    {
+    }
+
+    public function stopContainer()
+    {
+    }
 }
