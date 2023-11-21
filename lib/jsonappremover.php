c7d0bca41c537acb1f954087a914a2c68d05470c
CyberVitexus <info@vitexsoftware.cz>
2023-11-21 21:28:23 +0100

    json based App remover added

---
+++ lib/jsonappremover.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Multi Flexi - remove application by its json definition
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ * @copyright  2023 Vitex Software
+ */
+
+namespace MultiFlexi;
+
+require_once '../vendor/autoload.php';
+
+\Ease\Shared::init(['DB_CONNECTION', 'DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'], '../.env');
+$loggers = ['console', 'syslog', '\MultiFlexi\LogToSQL'];
+if (\Ease\Functions::cfg('ZABBIX_SERVER')) {
+    $loggers[] = '\MultiFlexi\LogToZabbix';
+}
+define('EASE_LOGGER', implode('|', $loggers));
+define('APP_NAME', 'MultiFlexi json app Remover');
+
+\Ease\Shared::user(new \Ease\Anonym());
+if (array_key_exists('1', $argv) && file_exists($argv[1])) {
+    $apper = new Application($argc == 3 ? intval($argv[3]) : null);
+    if (\Ease\Shared::cfg('APP_DEBUG')) {
+        $apper->logBanner();
+    }
+    if (empty($apper->JsonAppRemove($argv[1]))) {
+        $apper->addStatusMesssage(_('Error importing application json'), 'error');
+        exit(1);
+    }
+} else {
+    echo "usage: app.template.json [app id]";
+}
