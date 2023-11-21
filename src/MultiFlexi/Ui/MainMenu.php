213efb887cf018cab00bcfe66b2957d9ea0ce6fd
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2023-11-21 11:06:18 +0100

    Check and report if daemon service is not running

--- src/MultiFlexi/Ui/MainMenu.php
+++ src/MultiFlexi/Ui/MainMenu.php
@@ -14,13 +14,12 @@ namespace MultiFlexi\Ui;
  *
  * @author vitex
  */
-class MainMenu extends \Ease\Html\DivTag
-{
+class MainMenu extends \Ease\Html\DivTag {
+
     /**
      * Vytvoří hlavní menu.
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct(null, ['id' => 'MainMenu']);
     }
 
@@ -33,8 +32,7 @@ class MainMenu extends \Ease\Html\DivTag
      *
      * @return string
      */
-    protected function getMenuList($source, $icon = null, $nest = null)
-    {
+    protected function getMenuList($source, $icon = null, $nest = null) {
         $keycolumn = $source->getkeyColumn();
         $namecolumn = $source->nameColumn;
         $columns = [$source->getkeyColumn(), $namecolumn];
@@ -64,8 +62,7 @@ class MainMenu extends \Ease\Html\DivTag
     /**
      * Insert menu.
      */
-    public function afterAdd()
-    {
+    public function afterAdd() {
         $nav = $this->addItem(new BootstrapMenu('main-menu', null, ['class' => 'navbar navbar-expand-lg navbar-light bg-light']));
         if (\Ease\Shared::user()->isLogged()) { //Authenticated user
             $oPage = WebPage::singleton();
@@ -107,6 +104,10 @@ class MainMenu extends \Ease\Html\DivTag
 
             $nav->addDropDownMenu('<img height=30 src=images/log.svg> ' . _('Logs'), ['logs.php' => _('System'), 'joblist.php' => _('Jobs')]);
             $nav->addMenuItem(new \Ease\Html\ATag('logout.php', '<img height=30 src=images/application-exit.svg> ' . _('Sign Off')), 'right');
+
+            if (\MultiFlexi\Runner::isServiceActive('multiflexi') === false) {
+                $oPage->addStatusMessage(_('systemd service is not running. Consider `systemctl start multiflexi`'), 'warning');
+            }
         }
     }
 
@@ -115,16 +116,15 @@ class MainMenu extends \Ease\Html\DivTag
      * @param type $nav
      * @param type $apps
      */
-    public function appsMenuEnabled($nav, $apps)
-    {
+    public function appsMenuEnabled($nav, $apps) {
         $appsMenu = ['app.php' => _('Register Application')];
         if (!empty($apps)) {
             $appsMenu['apps.php'] = _('Application list');
         }
 
         $nav->addDropDownMenu(
-            '<img width=30 src=images/apps.svg> ' . _('Applications'),
-            array_merge($appsMenu, ['' => ''], $apps)
+                '<img width=30 src=images/apps.svg> ' . _('Applications'),
+                array_merge($appsMenu, ['' => ''], $apps)
         );
     }
 
@@ -134,16 +134,15 @@ class MainMenu extends \Ease\Html\DivTag
      * @param \Ease\Html\NavTag $nav
      * @param array $servers
      */
-    public function serversMenuEnabled($nav, $servers)
-    {
+    public function serversMenuEnabled($nav, $servers) {
         $serversMenu = ['server.php' => _('Register new Server')];
         if (!empty($servers)) {
             $serversMenu['servers.php'] = _('Instance list');
         }
 
         $nav->addDropDownMenu(
-            '<img width=30 src=images/server.svg> ' . _('Servers'),
-            array_merge($serversMenu, ['' => ''], $servers)
+                '<img width=30 src=images/server.svg> ' . _('Servers'),
+                array_merge($serversMenu, ['' => ''], $servers)
         );
     }
 
@@ -153,11 +152,10 @@ class MainMenu extends \Ease\Html\DivTag
      * @param \Ease\Html\NavTag $nav
      * @param array $companys
      */
-    public function companysMenuEnabled($nav, $companys)
-    {
+    public function companysMenuEnabled($nav, $companys) {
         $nav->addDropDownMenu(
-            '<img width=30 src=images/company.svg> ' . _('Companies'),
-            array_merge(['companysetup.php' => _('New Company')], ['' => ''], ['companys.php' => _('Listing')], $companys)
+                '<img width=30 src=images/company.svg> ' . _('Companies'),
+                array_merge(['companysetup.php' => _('New Company')], ['' => ''], ['companys.php' => _('Listing')], $companys)
         );
     }
 
@@ -165,8 +163,7 @@ class MainMenu extends \Ease\Html\DivTag
      *
      * @param \Ease\Html\NavTag $nav
      */
-    public function companysMenuDisabled($nav)
-    {
+    public function companysMenuDisabled($nav) {
         $nav->addMenuItem(new \Ease\Html\ATag('#', '<img width=30 src=images/company.svg> ' . _('Companies'), ['class' => 'nav-link disabled']));
     }
 
@@ -175,33 +172,30 @@ class MainMenu extends \Ease\Html\DivTag
      * @param type $nav
      * @param type $customers
      */
-    public function customersMenuEnabled($nav, $customers)
-    {
+    public function customersMenuEnabled($nav, $customers) {
         $customersMenu = ['customer.php' => _('New Customer')];
         $nav->addDropDownMenu(
-            '<img width=30 src=images/customer.svg> ' . _('Customers'),
-            array_merge($customersMenu, ['' => ''], $customers)
+                '<img width=30 src=images/customer.svg> ' . _('Customers'),
+                array_merge($customersMenu, ['' => ''], $customers)
         );
     }
 
     /**
      *
-     * @param type $nav
+     * @param \Ease\Html\NavTag $nav
      */
-    public function customersMenuDisabled($nav)
-    {
+    public function customersMenuDisabled($nav) {
         $nav->addMenuItem(new \Ease\Html\ATag('#', '<img width=30 src=images/customer.svg> ' . _('Customers'), ['class' => 'nav-link disabled']));
     }
 
     /**
      *
-     * @param type $nav
+     * @param \Ease\Html\NavTag $nav
      */
-    public function usersMenuEnabled($nav)
-    {
+    public function usersMenuEnabled($nav) {
         $nav->addDropDownMenu(
-            '<img width=30 src=images/system-users.svg> ' . _('Admin'),
-            array_merge([
+                '<img width=30 src=images/system-users.svg> ' . _('Admin'),
+                array_merge([
             'createaccount.php' => _('New Admin'),
             //            'users.php' => new \Ease\TWB4\Widgets\FaIcon('list') . '&nbsp;' . _('Admin Overview'),
             '' => '',
@@ -212,8 +206,7 @@ class MainMenu extends \Ease\Html\DivTag
     /**
      * Přidá do stránky javascript pro skrývání oblasti stavových zpráv.
      */
-    public function finalize()
-    {
+    public function finalize() {
 //        if (\Ease\Shared::user()->isLogged()) { //Authenticated user
 //            $this->addItem(new Breadcrumb());
 //        }
@@ -234,9 +227,9 @@ class MainMenu extends \Ease\Html\DivTag
             \Ease\Shared::logger()->cleanMessages();
             WebPage::singleton()->addCss('.dropdown-menu { overflow-y: auto } ');
             WebPage::singleton()->addJavaScript(
-                "$('.dropdown-menu').css('max-height',$(window).height()-100);",
-                null,
-                true
+                    "$('.dropdown-menu').css('max-height',$(window).height()-100);",
+                    null,
+                    true
             );
             WebPage::singleton()->includeJavaScript('js/slideupmessages.js');
         }
