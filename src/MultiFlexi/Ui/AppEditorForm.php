4c1ed36a1722fa27aec476f450b9bbe9d0f23056
CyberVitexus <info@vitexsoftware.cz>
2023-12-20 00:21:27 +0100

    Oci Image column renamed

--- src/MultiFlexi/Ui/AppEditorForm.php
+++ src/MultiFlexi/Ui/AppEditorForm.php
@@ -33,7 +33,7 @@ class AppEditorForm extends EngineForm
         $imgInput = $this->addInput(new InputFileTag('imageraw'), _('Application Icon'));
         $this->addInput(new Toggle('enabled', $this->engine->getDataValue('enabled') == 1), _('Enabled'));
         $this->addInput(new InputTextTag('homepage'), _('Application Homepage'));
-        $this->addInput(new InputTextTag('image'), _('Container image'));
+        $this->addInput(new InputTextTag('ociimage'), _('Container image'));
         $this->addInput(new InputTextTag('version'), _('Current Version of Application'));
         $this->addInput(new InputTextTag('deploy'), _('Application deploy command'));
         $this->addInput(new SubmitButton(_('Save'), 'success'));
