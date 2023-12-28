560c793474d063e09cb7f3fd1d2a5740724d8540
CyberVitexus <info@vitexsoftware.cz>
2023-12-28 15:13:49 +0100

    Try to fix Constrait

---
+++ db/migrations/20231228135418_token_id.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class TokenId extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        if ($this->adapter->getAdapterType() != 'sqlite') {
+        $table = $this->table('user');
+            $table
+                ->changeColumn('id', 'integer', ['identity' => true, 'signed' => false])
+                ->save();
+        }
+
+	$exists = $this->hasTable('token');
+        if ($exists) {
+    	    $table = $this->table('token');
+	    $table->drop()->save();
+	}
+
+        $table = $this->table('token');
+        $table
+                ->addColumn('token', 'string', ['limit' => 64])
+                ->addColumn('start', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
+                ->addColumn('until', 'datetime', ['null' => true])
+                ->addColumn('user_id', 'integer', ['null' => false, 'signed'=>false])
+                ->create();
+
+	
+
+    	$table->addForeignKey('user_id', 'user', 'id', ['constraint' => 'tokeuser_must_exist'])->save();
+
+
+    }
+}
