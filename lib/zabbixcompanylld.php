71cdb06973ff6ee7dc49b6fb2c3def42daa2b0ed
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2024-03-13 18:15:25 +0100

    Zabbix LLD now return all runtemplates

--- lib/zabbixcompanylld.php
+++ lib/zabbixcompanylld.php
@@ -25,36 +25,32 @@ Shared::user(new Anonym());
 
 $venue = array_key_exists(1, $argv) ? $argv[1] : '';
 
-if ($venue) {
-    $argParts = explode('.', $venue);
-    $companyCode = $argParts[count($argParts) - 1];
-    $server = str_replace('.' . $companyCode, '', $venue);
+$argParts = explode('.', $venue);
+$companyCode = $argParts[count($argParts) - 1];
+$server = str_replace('.' . $companyCode, '', $venue);
 
-    $lldData = [];
-    $ap2c = new \MultiFlexi\RunTemplate();
-    $companer = new Company(['code' => $companyCode], ['autoload' => true]);
-    $ca = new \MultiFlexi\CompanyApp($companer);
-    $apper = new Application();
+$lldData = [];
+$ap2c = new \MultiFlexi\RunTemplate();
+$companer = new Company(['code' => $companyCode], ['autoload' => true]);
+$ca = new \MultiFlexi\CompanyApp($companer);
+$apper = new Application();
 
-    $companyData = $companer->getData();
-    $appsAssigned = $ca->getAssigned()->leftJoin('apps ON apps.id = companyapp.app_id')->select(['apps.name', 'apps.description', 'apps.id', 'apps.image, apps.code, apps.uuid'], true)->fetchAll('id');
-    $runtemplates = $ap2c->getPeriodAppsForCompany($companer->getMyKey());
+$companyData = $companer->getData();
+$appsAssigned = $ca->getAssigned()->leftJoin('apps ON apps.id = companyapp.app_id')->select(['apps.name', 'apps.description', 'apps.id', 'apps.image, apps.code, apps.uuid'], true)->fetchAll('id');
+$runtemplates = $ap2c->getPeriodAppsForCompany($companer->getMyKey());
 
-    foreach ($runtemplates as $runtemplateData) {
+foreach ($runtemplates as $runtemplateData) {
 //        $apper->setData($app);
 //        $app['config'] = $apper->getAppEnvironmentFields();
-        $lldData[] = [
-            '{#APPNAME}' => $appsAssigned[$runtemplateData['app_id']]['name'],
-            '{#APPNAME_CODE}' => $appsAssigned[$runtemplateData['app_id']]['code'],
-            '{#APPNAME_UUID}' => $appsAssigned[$runtemplateData['app_id']]['uuid'],
-            '{#INTERVAL}' => Job::codeToInterval($runtemplateData['interv']),
-            '{#RUNTEMPLATE}' => $runtemplateData['id'],
-            '{#COMPANY_NAME}' => $companyData['name'],
-            '{#COMPANY_CODE}' => $companyData['code'],
-            '{#COMPANY_SERVER}' => \Ease\Shared::cfg('ZABBIX_HOST')
-        ];
-    }
-    echo json_encode($lldData, JSON_PRETTY_PRINT);
-} else {
-    die(_('SERVER.COMPANY_CODE parameter is requied'));
+    $lldData[] = [
+        '{#APPNAME}' => $appsAssigned[$runtemplateData['app_id']]['name'],
+        '{#APPNAME_CODE}' => $appsAssigned[$runtemplateData['app_id']]['code'],
+        '{#APPNAME_UUID}' => $appsAssigned[$runtemplateData['app_id']]['uuid'],
+        '{#INTERVAL}' => Job::codeToInterval($runtemplateData['interv']),
+        '{#RUNTEMPLATE}' => $runtemplateData['id'],
+        '{#COMPANY_NAME}' => $companyData['name'],
+        '{#COMPANY_CODE}' => $companyData['code'],
+        '{#COMPANY_SERVER}' => \Ease\Shared::cfg('ZABBIX_HOST')
+    ];
 }
+echo json_encode($lldData, JSON_PRETTY_PRINT);
