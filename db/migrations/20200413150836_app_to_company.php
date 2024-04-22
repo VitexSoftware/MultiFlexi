71c1990ef32e6477018a93462944f1e7d8931f41
CyberVitexus <info@vitexsoftware.cz>
2024-04-23 00:30:41 +0200

    #13 Unique key here is not good idea

--- db/migrations/20200413150836_app_to_company.php
+++ db/migrations/20200413150836_app_to_company.php
@@ -11,7 +11,7 @@ class AppToCompany extends AbstractMigration {
         $table->addColumn('app_id', 'integer', ['null' => false])
         ->addColumn('company_id', 'integer', ['null' => false])
         ->addColumn('interval','string',['length'=>1])
-        ->addIndex(['app_id', 'company_id'], ['unique' => true])
+        ->addIndex(['app_id', 'company_id'])
         ->addForeignKey('app_id', 'apps', ['id'], ['constraint' => 'a2p-app_must_exist'])
         ->addForeignKey('company_id', 'company', ['id'], ['constraint' => 'a2p-company_must_exist']);
         $table->save();
