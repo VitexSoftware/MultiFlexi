caa2796d8113336b3021dbf59911f706691df94b
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2024-04-22 20:38:38 +0200

    #13 - Clone form and cloner page

---
+++ src/runtemplateclone.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Multi Flexi - Run Template Clone page.
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ * @copyright  2024 Vitex Software
+ */
+
+namespace MultiFlexi\Ui;
+
+use Ease\WebPage;
+use MultiFlexi\RunTemplate;
+
+require_once './init.php';
+$oPage->onlyForLogged();
+
+$runTemplate = new RunTemplate(WebPage::getRequestValue('id', 'int'));
+$cloneName = \Ease\TWB4\WebPage::getRequestValue('clonename');
+
+$runTemplate->unsetDataValue($runTemplate->getKeyColumn());
+$runTemplate->setDataValue('name', $cloneName);
+try {
+    $cloneId = $runTemplate->insertToSQL();
+    $oPage->redirect('runtemplate.php?id=' . $cloneId);
+} catch (Exception $exc) {
+    $oPage->addItem(new PageTop(_('Runtemplate Clone')));
+    $oPage->addStatusMessage(_('Error creating runtemplate clone'), 'error');
+    $oPage->addItem(new PageBottom());
+    $oPage->draw();
+}
+
+
+
+
