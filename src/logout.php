41f8902f14f1ccdd2f0c009236fc06ad44005988
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2024-06-03 20:28:28 +0200

    Show random image on logout page

--- src/logout.php
+++ src/logout.php
@@ -21,15 +21,15 @@ if (is_null(\Ease\Shared::user()->getUserID()) === false) {
 $oPage->addItem(new PageTop(_('Sign Off')));
 
 $byerow = new \Ease\TWB4\Row();
-$byerow->addColumn(6);
+$byerow->addColumn(6, new \Ease\Html\ImgTag('images/openclipart/' . \MultiFlexi\Ui\OpenClipart::randomImage('images/openclipart')));
 $byeInfo = $byerow->addColumn(6, new \Ease\Html\H1Tag(_('Good bye')));
 
 $byeInfo->addItem('<br/><br/><br/><br/>');
 $byeInfo->addItem(new \Ease\Html\DivTag(new \Ease\Html\ATag(
-    'login.php',
-    _('Thank you for your patronage and look forward to another visit'),
-    ['class' => 'jumbotron']
-)));
+                        'login.php',
+                        _('Thank you for your patronage and look forward to another visit'),
+                        ['class' => 'jumbotron']
+        )));
 $byeInfo->addItem('<br/><br/><br/><br/>');
 
 $oPage->container->addItem($byerow);
