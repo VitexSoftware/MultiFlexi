246dbabe799e661949c192639d7d2eae4b56cc82
CyberVitexus <info@vitexsoftware.cz>
2024-09-07 22:17:51 +0200

    Show topics in AppList

--- src/applist.php
+++ src/applist.php
@@ -15,21 +15,43 @@ declare(strict_types=1);
 
 namespace MultiFlexi\Ui;
 
+use Ease\TWB4\LinkButton;
+use Ease\TWB4\Panel;
+use Ease\TWB4\Table;
+use MultiFlexi\Application;
+
 require_once './init.php';
-
 $oPage->onlyForLogged();
-
 $oPage->addItem(new PageTop(_('Applications')));
 
-$oPage->container->addItem(new DBDataTable(new \MultiFlexi\Application()));
+$apps = new Application();
+
+$allAppData = $apps->getAll();
+
+$fbtable = new Table();
+$fbtable->addRowHeaderColumns([_('ID'), _('Enabled'), _('Image'), _('Name'), _('Description'), _('Executable'), _('Created'), _('Modified'), _('HomePage'), _('Requirements'), _('Container Image'), _('Version'), _('Code'), _('uuid')]);
+
+foreach ($allAppData as $appData) {
+    $appData['image'] = new \Ease\Html\ImgTag('appimage.php?uuid='.$appData['uuid'], _('Icon'), ['height' => 40]);
+    $appData['enabled'] = ($appData['enabled'] === 1 ? '✔' : '❌');
+    $executablePath = Application::findBinaryInPath($appData['executable']);
+    $appData['executable'] = empty($executablePath) ? '<span title="'._('Command not found').'">⁉</span> '.$appData['executable'] : $executablePath;
+
+    if (empty($appData['setup']) === false) {
+        $initPath = Application::findBinaryInPath($appData['setup']);
+        $appData['setup'] = (empty($initPath) ? '<span title="'._('Command not found').'">⁉</span> '.$appData['setup'] : $initPath);
+    }
+
+    $appData['homepage'] = new \Ease\Html\ATag($appData['homepage'], $appData['homepage']);
+    $appData['name'] = new \Ease\Html\ATag('app.php?id='.$appData['id'], _($appData['name']));
+    $appData['description'] = _($appData['description']);
+    unset($appData['setup'], $appData['cmdparams'], $appData['deploy']);
+
+    $fbtable->addRowColumns($appData);
+}
+
+$oPage->container->addItem(new Panel(_('Applications'), 'default', $fbtable, new LinkButton('app.php', _('Register new'))));
+
 $oPage->addItem(new PageBottom());
-
-$oPage->addJavaScript(<<<'EOD'
-
-    setInterval(function () {
-      Molecule.ajax.reload();
-}, 300000);
-
-EOD);
 
 $oPage->draw();
