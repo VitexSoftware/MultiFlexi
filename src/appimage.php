05e5b1f300ea11f22280602a173cf2915bfbad3c
Vitex <info@vitexsoftware.cz>
2024-11-01 07:34:31 +0100

    Look for app icon in images/UUID.svg first

--- src/appimage.php
+++ src/appimage.php
@@ -20,23 +20,27 @@ require_once './init.php';
 $oPage->onlyForLogged();
 
 header('Cache-Control: max-age=31536000'); // Cache for 1 year
-header('Expires: '.gmdate('D, d M Y H:i:s', time() + 31536000).' GMT'); // Expires in 1 year
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 31536000) . ' GMT'); // Expires in 1 year
 
-$app = new \MultiFlexi\Application();
 
 $uuid = WebPage::getRequestValue('uuid');
 
-$image = $app->listingQuery()->select('image', true)->where('uuid', $uuid)->limit(1)->fetch('image');
-
+if (file_exists('images/' . $uuid . '.svg')) {
+    $contentType = 'image/svg+xml';
+    $imageData = file_get_contents('images/' . $uuid . '.svg');
+} else {
+    $app = new \MultiFlexi\Application();
+    $image = $app->listingQuery()->select('image', true)->where('uuid', $uuid)->limit(1)->fetch('image');
 // Extract content/type from data URI
-[$contentType, $base64Data] = explode(',', $image);
-[, $contentType] = explode(':', $contentType);
+    [$contentType, $base64Data] = explode(',', $image);
+    [, $contentType] = explode(':', $contentType);
 
 // Convert base64 data to original format
-$imageData = base64_decode($base64Data, true);
+    $imageData = base64_decode($base64Data, true);
+}
 
 // Set proper content-type header
-header('Content-Type: '.str_replace(';base64', '', $contentType));
+header('Content-Type: ' . str_replace(';base64', '', $contentType));
 
 // Send image data to the browser
 echo $imageData;
