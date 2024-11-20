0ab9c386d095091ffd816b02159565bb5c147eb1
CyberVitexus <info@vitexsoftware.cz>
2024-11-20 23:52:56 +0100

    do not use anacron jobs do not use abraflexi functions in main program code

--- debian/multiflexi.cron.d
+++ debian/multiflexi.cron.d
@@ -1,4 +1,20 @@
-#Run AbraFlexi scripts once per year
-0 0 1 1 *	www-data multiflexi-scheduler y
 #Schedule ad-hoc actions
-* * * * *	www-data multiflexi-scheduler i
+* * * * *	multiflexi multiflexi-scheduler i
+
+#Schedule AbraFlexi scripts once per hour
+0 * * * *	multiflexi multiflexi-scheduler h
+
+#Schedule AbraFlexi scripts once per day
+0 0 * * *	multiflexi multiflexi-scheduler d
+
+#Schedule AbraFlexi scripts once per week
+0 0 * * 0	multiflexi multiflexi-scheduler w
+
+#Schedule AbraFlexi scripts once per month
+0 0 1 * *	multiflexi multiflexi-scheduler m
+
+#Schedule AbraFlexi scripts once per quarter
+0 0 1 1,4,7,10 *	multiflexi multiflexi-scheduler q
+
+#Run AbraFlexi scripts once per year
+0 0 1 1 *	multiflexi multiflexi-scheduler y
