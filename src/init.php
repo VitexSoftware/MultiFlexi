c5a0577e543d31f36a2b28662d6da172a3525a38
CyberVitexus <info@vitexsoftware.cz>
2024-11-22 05:12:29 +0100

    Timezone Setting Hotfix

--- src/init.php
+++ src/init.php
@@ -46,7 +46,7 @@ date_default_timezone_set('Europe/Prague');
 $script_tz = date_default_timezone_get();
 
 if (strcmp($script_tz, \ini_get('date.timezone'))) {
-    echo 'Script timezone differs from ini-set timezone.';
+//    echo 'Script timezone differs from ini-set timezone.';
 } else {
-    echo 'Script timezone and ini-set timezone match.';
+//    echo 'Script timezone and ini-set timezone match.';
 }
