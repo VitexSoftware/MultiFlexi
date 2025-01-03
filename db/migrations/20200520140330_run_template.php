c27350fa621aa3051258355747e0d9403d21f35d
Vítězslav Dvořák <info@vitexsoftware.cz>
2025-01-03 13:11:07 +0100

    create runtemplate table only when does not exists yet

--- db/migrations/20200520140330_run_template.php
+++ db/migrations/20200520140330_run_template.php
@@ -24,22 +24,25 @@ final class RunTemplate extends AbstractMigration
         $databaseType = $this->getAdapter()->getOption('adapter');
         $unsigned = ($databaseType === 'mysql') ? ['signed' => false] : [];
 
-        // Create the runtemplate table
-        $table = $this->table('runtemplate', ['id' => false, 'primary_key' => ['id']]);
-        $table->addColumn('id', 'integer', array_merge(['null' => false, 'identity' => true], $unsigned))
-            ->addColumn('app_id', 'integer', array_merge(['null' => false], $unsigned))
-            ->addColumn('company_id', 'integer', array_merge(['null' => false], $unsigned))
-            ->addColumn('interv', 'string', ['limit' => 1, 'null' => false])
-            ->addColumn('prepared', 'boolean', ['default' => null, 'null' => true])
-            ->addColumn('success', 'string', ['limit' => 250, 'default' => null, 'null' => true])
-            ->addColumn('fail', 'string', ['limit' => 250, 'default' => null, 'null' => true])
-            ->addColumn('name', 'string', ['limit' => 250, 'default' => null, 'null' => true])
-            ->addColumn('delay', 'integer', ['default' => 0, 'null' => false, 'comment' => 'Time before job is started after periodic scheduler run'])
-            ->addColumn('executor', 'string', ['limit' => 255, 'default' => 'Native', 'null' => false, 'comment' => 'Preferred Executor'])
-            ->addIndex(['company_id'], ['name' => 'a2p-company_must_exist'])
-            ->addIndex(['app_id', 'company_id'], ['name' => 'app_id', 'type' => 'btree'])
-            ->addForeignKey('app_id', 'apps', 'id', ['constraint' => 'a2p-app_must_exist'])
-            ->addForeignKey('company_id', 'company', 'id', ['constraint' => 'a2p-company_must_exist'])
-            ->create();
+        // Check if the table already exists
+        if (!$this->hasTable('runtemplate')) {
+            // Create the runtemplate table
+            $table = $this->table('runtemplate', ['id' => false, 'primary_key' => ['id']]);
+            $table->addColumn('id', 'integer', array_merge(['null' => false, 'identity' => true], $unsigned))
+                ->addColumn('app_id', 'integer', array_merge(['null' => false], $unsigned))
+                ->addColumn('company_id', 'integer', array_merge(['null' => false], $unsigned))
+                ->addColumn('interv', 'string', ['limit' => 1, 'null' => false])
+                ->addColumn('prepared', 'boolean', ['default' => null, 'null' => true])
+                ->addColumn('success', 'string', ['limit' => 250, 'default' => null, 'null' => true])
+                ->addColumn('fail', 'string', ['limit' => 250, 'default' => null, 'null' => true])
+                ->addColumn('name', 'string', ['limit' => 250, 'default' => null, 'null' => true])
+                ->addColumn('delay', 'integer', ['default' => 0, 'null' => false, 'comment' => 'Time before job is started after periodic scheduler run'])
+                ->addColumn('executor', 'string', ['limit' => 255, 'default' => 'Native', 'null' => false, 'comment' => 'Preferred Executor'])
+                ->addIndex(['company_id'], ['name' => 'a2p-company_must_exist'])
+                ->addIndex(['app_id', 'company_id'], ['name' => 'app_id', 'type' => 'btree'])
+                ->addForeignKey('app_id', 'apps', 'id', ['constraint' => 'a2p-app_must_exist'])
+                ->addForeignKey('company_id', 'company', 'id', ['constraint' => 'a2p-company_must_exist'])
+                ->create();
+        }
     }
 }
