95f31b26a64b78b98380d8a1d73adc78f2c37441
CyberVitexus <info@vitexsoftware.cz>
2025-01-15 10:10:23 +0100

    job output iporved

--- src/joboutput.php
+++ src/joboutput.php
@@ -33,4 +33,4 @@ header('Expires: 0');
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Pragma: public');
 header('Content-Length: '.mb_strlen($output));
-echo $output;
+echo stripslashes($output);
