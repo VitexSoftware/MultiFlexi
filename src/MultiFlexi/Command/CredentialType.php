5ccbf223d30432d858af8a7dc3eacb6a450a27b9
CyberVitexus <info@vitexsoftware.cz>
2025-01-19 18:28:51 +0100

    skeletons of new commandline operations added

---
+++ src/MultiFlexi/Command/CredentialType.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * MultiFlexi - 
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ * @copyright  2020 Vitex Software
+ */
+
+namespace MultiFlexi\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Description of CredentialType
+ *
+ * @author Vitex <info@vitexsoftware.cz> 
+ */
+class CredentialType  extends Command {
+    protected function configure(): void
+    {
+        $this
+            ->setName('credtype')
+            ->setDescription('Credential type operations')
+            ->addOption('--format', '-f', InputOption::VALUE_OPTIONAL, 'The output format: text or json. Defaults to text.', 'text')
+            ->setHelp('This command manage Credential Types');
+    }
+}
