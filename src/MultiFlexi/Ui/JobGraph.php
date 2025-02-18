7276ad450f1a0cdb9dbe6109f68b13a457e72dc9
CyberVitexus <info@vitexsoftware.cz>
2025-02-18 16:38:16 +0100

    Jobs Chart added

---
+++ src/MultiFlexi/Ui/JobGraph.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the MultiFlexi package
+ *
+ * https://multiflexi.eu/
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MultiFlexi\Ui;
+
+class JobGraph
+{
+    private $width;
+    private $height;
+    private $todaysJobs;
+    private $image;
+    private $successCount = 0;
+    private $failureCount = 0;
+    private $noExecutableCount = 0;
+    private $exceptionCount = 0;
+    private $waitingCount = 0;
+
+    public function __construct(int $width, int $height, array $todaysJobs)
+    {
+        $this->width = $width;
+        $this->height = $height;
+        $this->todaysJobs = $todaysJobs;
+        $this->image = imagecreatetruecolor($this->width, $this->height);
+
+        // Allocate colors
+        $white = imagecolorallocate($this->image, 255, 255, 255);
+        imagefill($this->image, 0, 0, $white);
+    }
+
+    public function generateImage(): void
+    {
+        $centerX = $this->width / 2;
+        $centerY = $this->height / 2;
+        $x = $centerX;
+        $y = $centerY;
+        $dx = 1;
+        $dy = 0;
+        $segment_length = 1;
+        $segment_passed = 0;
+        $totalJobs = \count($this->todaysJobs);
+
+        foreach ($this->todaysJobs as $index => $job) {
+            // Calculate color intensity based on the age of the data
+            $intensity = 128 + (int) (127 * ($index / $totalJobs)); // Make older data pixels darker
+            $green = imagecolorallocate($this->image, 0, $intensity, 0);
+            $red = imagecolorallocate($this->image, $intensity, 0, 0);
+            $yellow = imagecolorallocate($this->image, $intensity, $intensity, 0);
+            $black = imagecolorallocate($this->image, 0, 0, 0);
+            $blue = imagecolorallocate($this->image, 0, 0, $intensity);
+
+            if ($job['exitcode'] === 0) {
+                $color = $green;
+                ++$this->successCount;
+            } elseif ($job['exitcode'] === 127) {
+                $color = $yellow;
+                ++$this->noExecutableCount;
+            } elseif ($job['exitcode'] === 255) {
+                $color = $black;
+                ++$this->exceptionCount;
+            } elseif (null === $job['exitcode']) {
+                $color = $blue;
+                ++$this->waitingCount;
+            } else {
+                $color = $red;
+                ++$this->failureCount;
+            }
+
+            imagesetpixel($this->image, $x, $y, $color);
+
+            $x += $dx;
+            $y += $dy;
+            ++$segment_passed;
+
+            if ($segment_passed === $segment_length) {
+                $segment_passed = 0;
+
+                // Change direction
+                $temp = $dx;
+                $dx = -$dy;
+                $dy = $temp;
+
+                // Increase segment length after two segments
+                if ($dy === 0) {
+                    ++$segment_length;
+                }
+            }
+
+            // Stop if we reach the edge of the image
+            if ($x < 0 || $x >= $this->width || $y < 0 || $y >= $this->height) {
+                break;
+            }
+        }
+    }
+
+    public function getBase64Image(): string
+    {
+        // Capture the image output
+        ob_start();
+        imagepng($this->image);
+        $imageData = ob_get_contents();
+        ob_end_clean();
+        imagedestroy($this->image);
+
+        // Encode the image data as base64
+        return base64_encode($imageData);
+    }
+
+    public function getSuccessCount(): int
+    {
+        return $this->successCount;
+    }
+
+    public function getFailureCount(): int
+    {
+        return $this->failureCount;
+    }
+
+    public function getNoExecutableCount(): int
+    {
+        return $this->noExecutableCount;
+    }
+
+    public function getExceptionCount(): int
+    {
+        return $this->exceptionCount;
+    }
+
+    public function getWaitingCount(): int
+    {
+        return $this->waitingCount;
+    }
+
+    public function getTotalJobs(): int
+    {
+        return \count($this->todaysJobs);
+    }
+}
--- src/company.php
+++ src/company.php
@@ -90,7 +90,53 @@ foreach ($jobs as $job) {
 }
 
 $companyPanelContents[] = new CompanyAppsBar($companies);
-$companyPanelContents[] = new CompanyJobChart($jobber, ['id' => 'container']);
+$companyJobChart = new CompanyJobChart($jobber, ['id' => 'container']);
+
+$jobber = new \MultiFlexi\Job();
+
+$width = 200;
+$height = 200;
+
+$todaysJobs = $jobber->listingQuery()->select('exitcode', true)->limit($width * $height)->orderBy('id')->where('company_id', $companies->getMyKey())->fetchAll();
+
+$jobGraph = new JobGraph($width, $height, $todaysJobs);
+$jobGraph->generateImage();
+$base64Image = $jobGraph->getBase64Image();
+
+$imageTag = new \Ease\Html\ImgTag('data:image/png;base64,'.$base64Image, 'Job Success/Failure Graph', ['width' => $width, 'height' => $height]);
+
+// Calculate percentages
+$totalJobs = $jobGraph->getTotalJobs();
+$successCount = $jobGraph->getSuccessCount();
+$failureCount = $jobGraph->getFailureCount();
+$noExecutableCount = $jobGraph->getNoExecutableCount();
+$exceptionCount = $jobGraph->getExceptionCount();
+$waitingCount = $jobGraph->getWaitingCount();
+$successPercentage = ($totalJobs > 0) ? ($successCount / $totalJobs) * 100 : 0;
+$failurePercentage = ($totalJobs > 0) ? ($failureCount / $totalJobs) * 100 : 0;
+$noExecutablePercentage = ($totalJobs > 0) ? ($noExecutableCount / $totalJobs) * 100 : 0;
+$exceptionPercentage = ($totalJobs > 0) ? ($exceptionCount / $totalJobs) * 100 : 0;
+$waitingPercentage = ($totalJobs > 0) ? ($waitingCount / $totalJobs) * 100 : 0;
+
+// Create the legend
+$legend = new \Ease\Html\DivTag(
+    [
+        new \Ease\Html\DivTag(new \Ease\Html\SpanTag('■', ['style' => 'color: rgb(0, 255, 0);']).' Success: '.$successCount.' ('.number_format($successPercentage, 2).'%)'),
+        new \Ease\Html\DivTag(new \Ease\Html\SpanTag('■', ['style' => 'color: rgb(255, 0, 0); margin-left: 10px;']).' Failure: '.$failureCount.' ('.number_format($failurePercentage, 2).'%)'),
+        new \Ease\Html\DivTag(new \Ease\Html\SpanTag('■', ['style' => 'color: rgb(255, 255, 0); margin-left: 10px;']).' No Executable: '.$noExecutableCount.' ('.number_format($noExecutablePercentage, 2).'%)'),
+        new \Ease\Html\DivTag(new \Ease\Html\SpanTag('■', ['style' => 'color: rgb(0, 0, 0); margin-left: 10px;']).' Exception: '.$exceptionCount.' ('.number_format($exceptionPercentage, 2).'%)'),
+        new \Ease\Html\DivTag(new \Ease\Html\SpanTag('■', ['style' => 'color: rgb(0, 0, 255); margin-left: 10px;']).' Waiting: '.$waitingCount.' ('.number_format($waitingPercentage, 2).'%)'),
+        new \Ease\Html\DivTag('Total Jobs: '.$totalJobs, ['style' => 'margin-top: 10px;']),
+    ],
+    ['style' => 'margin-top: 20px;'],
+);
+
+$graphRow = new \Ease\TWB4\Row();
+$graphRow->addColumn(2, [$imageTag, $legend]);
+$graphRow->addColumn(10, $companyJobChart);
+
+$companyPanelContents[] = $graphRow;
+
 $companyPanelContents[] = new \Ease\Html\H3Tag(_('job queue'));
 $companyPanelContents[] = $jobList;
 $bottomLine = new CompanyDbStatus($companies);
