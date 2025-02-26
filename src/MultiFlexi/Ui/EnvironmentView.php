5b0ca5d88bd7197ca78cbb64422c6b142e1ab3c8
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2025-02-26 18:24:37 +0100

    Show job .env export on dedicated tab

--- src/MultiFlexi/Ui/EnvironmentView.php
+++ src/MultiFlexi/Ui/EnvironmentView.php
@@ -57,10 +57,12 @@ class EnvironmentView extends \Ease\Html\TableTag
             if (\array_key_exists('credential_id', $envData)) {
                 $source = new \Ease\Html\DivTag(new \Ease\Html\ATag('credential.php?id='.$envData['credential_id'], $envData['source']));
                 $credTyper = new \MultiFlexi\CredentialType($envData['credential_type']);
-                if($credTyper->getDataValue('logo')){
-                   $credTyper->addStatusMessage(sprintf(_('There is no Logo defined for %s Credential Types'), $envData['credential_type']), 'warning'); 
+
+                if ($credTyper->getDataValue('logo')) {
+                    $credTyper->addStatusMessage(sprintf(_('There is no Logo defined for %s Credential Types'), $envData['credential_type']), 'warning');
                 }
-                $source->addItem(new \Ease\Html\ImgTag((string)$credTyper->getDataValue('logo'), $envData['credential_type'], ['title' => $credTyper->getRecordName(), 'height' => '30', 'align' => 'right']));
+
+                $source->addItem(new \Ease\Html\ImgTag((string) $credTyper->getDataValue('logo'), $envData['credential_type'], ['title' => $credTyper->getRecordName(), 'height' => '30', 'align' => 'right']));
             } else {
                 $ns = \array_key_exists('source', $envData) ? explode('\\', $envData['source']) : ['n/a'];
                 $source = end($ns);
