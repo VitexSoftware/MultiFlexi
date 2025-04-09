614309e6e936e5afd9b01f251fcaa36b19280252
CyberVitexus <info@vitexsoftware.cz>
2025-04-09 16:26:02 +0200

    do not fail when ZabbixSender class is not present

--- src/MultiFlexi/LogToSQL.php
+++ src/MultiFlexi/LogToSQL.php
@@ -118,7 +118,7 @@ class LogToSQL extends \Ease\SQL\Engine implements \Ease\Logger\Loggingable
                 if (method_exists($caller, 'getObjectName')) {
                     $venue = $caller->getObjectName();
                 } else {
-                    $venue = get_class($caller); // Použijeme get_class() místo ::class
+                    $venue = $caller::class; // Použijeme get_class() místo ::class
                 }
 
                 break;
