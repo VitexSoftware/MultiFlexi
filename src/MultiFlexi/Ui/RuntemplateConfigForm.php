439fc65f267013f81b629c112eb845eacf592cc9
CyberVitexus <info@vitexsoftware.cz>
2025-07-01 16:11:47 +0200

    Enhances setup command alert with icon
    Adds an informational icon to the setup command alert for improved
    visual clarity and user experience. No functional changes to logic.

--- src/MultiFlexi/Ui/RuntemplateConfigForm.php
+++ src/MultiFlexi/Ui/RuntemplateConfigForm.php
@@ -105,9 +105,9 @@ class RuntemplateConfigForm extends EngineForm
         
         $appSetupCommand = $engine->getApplication()->getDataValue('setup');
         if (!empty($appSetupCommand)) {
-            $saveColumn->addItem(new \Ease\TWB4\Alert('info', _('After saving configuration, the following setup command will be executed:').'<br><code>'.$appSetupCommand.'</code>'));
+            $saveColumn->addItem(new \Ease\TWB4\Alert('info', 'ℹ️&nbsp;' ._('After saving configuration, the following setup command will be executed:').'<br><code>'.$appSetupCommand.'</code>'));
         }
-        
+
         $this->addItem($saveRow);
         
         
