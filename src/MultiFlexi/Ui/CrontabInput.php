065de18c5e64867c8f8bb2c37b9b7d68958f8267
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2025-08-04 13:55:10 +0200

    Refactors CrontabInput and MainMenu classes for improved readability and consistency; cleans up RunTemplatePanel by removing unnecessary whitespace.

--- src/MultiFlexi/Ui/CrontabInput.php
+++ src/MultiFlexi/Ui/CrontabInput.php
@@ -1,20 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * MultiFlexi - 
+ * This file is part of the MultiFlexi package
  *
- * @author Vítězslav Dvořák <info@vitexsoftware.cz>
- * @copyright  2020 Vitex Software
+ * https://multiflexi.eu/
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace MultiFlexi\Ui;
 
 /**
- * Description of CrontabInput
+ * Description of CrontabInput.
  *
- * @author Vitex <info@vitexsoftware.cz> 
+ * @author Vitex <info@vitexsoftware.cz>
  */
-use Ease\Html\DivTag;
 
 /**
  * CrontabInput renders a cron expression input field with validation and UI enhancements.
@@ -24,25 +29,25 @@ use Ease\Html\DivTag;
  *
  * @author Vitex <info@vitexsoftware.cz>
  */
-class CrontabInput extends \Ease\Html\PairTag {
-
+class CrontabInput extends \Ease\Html\PairTag
+{
     /**
      * Construct a cron expression input field.
      *
-     * @param string $name        The input field name and id.
-     * @param string $value       The initial cron expression value.
-     * @param array  $attributes  Additional HTML attributes for the input.
+     * @param string $name       the input field name and id
+     * @param string $value      the initial cron expression value
+     * @param array  $attributes additional HTML attributes for the input
      */
-    public function __construct(string $name, string $value = '', array $attributes = []) {
+    public function __construct(string $name, string $value = '', array $attributes = [])
+    {
         parent::__construct('cron-expression-input', ['color' => 'd58512', 'value' => $value]);
     }
 
     /**
      * Include required JS and CSS for the cron expression input.
-     *
-     * @return void
      */
-    public static function includeAssets(): void {
+    public static function includeAssets(): void
+    {
         // These should be called from the page controller or panel
         \Ease\WebPage::singleton()->includeCss('css/cron-expression-input.min.css');
         \Ease\WebPage::singleton()->includeJavaScript('js/cron-expression-input.min.js');
