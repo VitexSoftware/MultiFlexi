04e1923278cd70324bb84e0cab3455b8fedab5f9
CyberVitexus <info@vitexsoftware.cz>
2026-03-04 02:32:20 +0100

    fix: create standalone autoload.php for Debian packaging

---
+++ debian/autoload.php
@@ -0,0 +1,9 @@
+<?php
+// Debian autoloader for multiflexi
+// Load dependency autoloaders
+require_once '/usr/share/php/MultiFlexi/autoload.php';
+require_once '/usr/share/php/Ease/autoload.php';
+require_once '/usr/share/php/EaseTWB4/autoload.php';
+require_once '/usr/share/php/EaseFluentPDO/autoload.php';
+require_once '/usr/share/php/EaseTWB4Widgets/autoload.php';
+require_once '/usr/share/php/EaseHtmlWidgets/autoload.php';
